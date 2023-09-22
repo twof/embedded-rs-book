d52eca526cc3047e4fe7ec9a05e7faa2d707cf3b
twof <fabiobean2@gmail.com>
2023-09-22 04:20:18 -0700

    writing strings to the serial comms

--- microbit/src/07-uart/src/main.rs
+++ microbit/src/07-uart/src/main.rs
@@ -2,26 +2,16 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
 
-#[cfg(feature = "v1")]
-use microbit::{
-    hal::prelude::*,
-    hal::uart,
-    hal::uart::{Baudrate, Parity},
-};
-
-#[cfg(feature = "v2")]
 use microbit::{
     hal::prelude::*,
     hal::uarte,
     hal::uarte::{Baudrate, Parity},
 };
 
-#[cfg(feature = "v2")]
 mod serial_setup;
-#[cfg(feature = "v2")]
 use serial_setup::UartePort;
 
 #[entry]
@@ -29,17 +19,6 @@ fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    #[cfg(feature = "v1")]
-    let mut serial = {
-        uart::Uart::new(
-            board.UART0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        )
-    };
-
-    #[cfg(feature = "v2")]
     let mut serial = {
         let serial = uarte::Uarte::new(
             board.UARTE0,
@@ -50,7 +29,10 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    nb::block!(serial.write(b'X')).unwrap();
+    for char in "The quick brown fox jumps over the lazy dog.".bytes() {
+        nb::block!(serial.write(char)).unwrap();
+    }
+
     nb::block!(serial.flush()).unwrap();
 
     loop {}
